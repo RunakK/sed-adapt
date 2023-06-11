a2800334585a692b0c6acb639dc0481742f595bc
RunakK <101060372+RunakK@users.noreply.github.com>
2023-06-11 13:45:49 +0300

    создание фавиконок и спрайт svg

--- gulpfile.js
+++ gulpfile.js
@@ -85,8 +85,8 @@ const sprite = () => {
 const copy = (done) => {
 gulp.src([
   'source/fonts/**/*.{woff2,woff}',
-  'source/*.ico'
-  // 'source/manifest.webmanifest'
+  'source/*.ico',
+  'source/manifest.webmanifest'
   ], {
   base: 'source'
   })
